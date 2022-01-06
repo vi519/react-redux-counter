4d1e93183b8da4c61dcd2b449f648b6d764b59fd
vi519 <68097369+vi519@users.noreply.github.com>
2022-01-06 14:16:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import {useSelector,useDispatch} from 'react-redux'
+import {incNumber,decNumber}  from './actions/index'
+
 
 function App() {
+
+  const myState= useSelector((state)=>state.changeTheNumber)
+const dispatch =useDispatch();
+ const  handleChange=()=>{
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <h1>Redux Demo</h1>
+  <div>
+    <button onClick={()=>dispatch(decNumber())}>+</button>
+    <input type="text" onChange={handleChange} style={{textAlign:"center"}}value={myState}/>
+    <button onClick={()=>dispatch(incNumber())}>-</button>
+  </div>
+
     </div>
   );
 }
